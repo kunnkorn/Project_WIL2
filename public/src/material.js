f2374bcf4da6256acaac6fdf29cb7810d16adb67
Thidawan <aimaim.3112@gmail.com>
2021-03-12 19:21:29 +0700

    table material

--- public/src/material.js
+++ public/src/material.js
@@ -3,6 +3,44 @@ $(document).ready(function(){
      $('#sidebarCollapse').on('click', function () {
         $('#sidebar, #content').toggleClass('active');
     });
+    var rowID;
+    var material = [{
+        "number": "1",
+        "id": "101000001",
+        "name": "กรรไกรตัดกระดาษ 6",
+        "balance": "50",
+        "unit": "อัน",
+    }, {
+        "number": "2",
+        "id": "101000002",
+        "name": "กรรไกรตัดกระดาษ 9",
+        "balance": "50",
+        "unit": "อัน",
+    }, {
+        "number": "3",
+        "id": "101000003",
+        "name": "กระดาษ POSTIT ขนาด 2*3",
+        "balance": "110",
+        "unit": "เล่ม",
+    }];
+    var table = $("#materialTable").DataTable({
+        responsive: true,       //for responsive column display
+        deferRender: true,      //if large data, use this option
+
+        data: material,
+        columns: [
+            { data: "number", title: "ลำดับ" },
+            { data: "id", title: "รหัสวัสดุ" },
+            { data: "name", title: "รายการ" },
+            { data: "balance", title: "คงเหลือ" },
+            { data: "unit", title: "หน่ายนับ" },
+            { title: "เพิ่มจำนวน", orderable: false, defaultContent: " <button class='btn btn-warning my-1'>Edit</button>" }
+        ],
+        columnDefs: [
+            // make the last column align right, also target: "_all"
+            { "className": "dt-center", "targets": 5 }
+        ]
+    });
 
     
 
