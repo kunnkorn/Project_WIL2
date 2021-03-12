61c9f22c401e679de94b0d8094cf96926a2c04e5
Thidawan <aimaim.3112@gmail.com>
2021-03-12 19:18:31 +0700

    css material & nav

---
+++ public/src/material.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+     // Sidebar toggle behavior
+     $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+    });
+
+    
+
+});
