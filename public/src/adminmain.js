ef687f5db9ce38b4d106ada488bb8dd6a93467dc
PunKongsomboon <ksm_punn@hotmail.com>
2021-04-21 02:52:36 +0700

    service material table

--- public/src/adminmain.js
+++ public/src/adminmain.js
@@ -1,47 +1,87 @@
+function init() {
+    gapi.load('auth2', () => {
+        gapi.auth2.init({
+            client_id: '565819629218-hrjqptqk34lk5sq2599tasa7gc2tho24.apps.googleusercontent.com'
+        });
+    });
+}
+
+function signout() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+        window.location.replace('/logout');
+    });
+}
+
+
 $(document).ready(function () {
+    var table;
 
-
-
-    var rowID;
-    var material = [{
-        "number": "1",
-        "date": "17/05/2564",
-        "name": "นาย บันเทิง สุดสุด",
-        "order": "64050001",
-        "status": "รออนุมัติ"
-    }, {
-        "number": "2",
-        "date": "19/05/2564",
-        "name": "นางสาว สมหญิง มากมาก",
-        "order": "64050010",
-        "status": "รออนุมัติ"
-    }, {
-        "number": "3",
-        "date": "21/05/2564",
-        "name": "นาย อนุรักษ์ ผืนป่า",
-        "order": "64050013",
-        "status": "รออนุมัติ"
-    }];
-    var table = $("#materialTable").DataTable({
-        responsive: true,       //for responsive column display
-        deferRender: true,      //if large data, use this option
-
-        data: material,
+    table = $("#requistionTable").DataTable({
+        responsive: true,
+        ajax: {
+            method: 'GET',
+            url: '/getrequisition',
+            dataSrc: function (data) {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].status_requisition == 1) {
+                        data[i].status_requisition = "รออนุมัติ";
+                    } else if (data[i].status_requisition == 2) {
+                        data[i].status_requisition = "อนุมัติ";
+                    }
+                }
+                return data;
+            }
+        },
         columns: [
-            { data: "number", title: "ลำดับ" },
-            { data: "date", title: "วันที่" },
+            { data: "date_requisition", title: "วันที่" },
             { data: "name", title: "ชื่อ-นามสกุล" },
-            { data: "order", title: "เลขที่ใบเบิก" },
-            { data: "status", title: "สถานะ" },
-            { title: "รายละเอียด", orderable: false, defaultContent: "<a href='detailreq.html'>รายละเอียดเพิ่มเติม</a>" }
+            { data: "requisition_id", title: "เลขที่ใบเบิก" },
+            { data: "status_requisition", title: "สถานะ" },
+            { title: "รายระเอียด", defaultContent: "<a class='btndetail'>รายละเอียดเพิ่มเติม</a>" },
         ],
-        columnDefs: [
-            // make the last column align right, also target: "_all"
-            { "className": "dt-center", "targets": 5 }
-        ]
+        "order": [[1, 'asc']]
     });
 
-    
+    table.on('order.dt search.dt', function () {
+        table.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
+            cell.innerHTML = i + 1;
+        });
+    }).draw();
+
+    // $.ajax({
+    //     type: 'GET',
+    //     url: '/getrequisition',
+    //     success: (response) => {
+    //         table = $("#requistionTable").DataTable({
+    //             responsive: true,
+    //             dataSrc: function (data) {
+    //                 for (let i = 0; i < data.length; i++) {
+    //                     if (data[i].status_requisiton == 1) {
+    //                         data[i].status_requisiton = "รออนุมัติ";
+    //                     } else if (data[i].status_requisiton == 2) {
+    //                         data[i].status_requisiton = "อนุมัติ";
+    //                     }
+    //                 }
+    //             },
+    //             columns: [
+    //                 { data: "date_requisition", title: "วันที่" },
+    //                 { data: "name", title: "ชื่อ-นามสกุล" },
+    //                 { data: "requisition_id", title: "เลขที่ใบเบิก" },
+    //                 { data: "status_requisition", title: "สถานะ" },
+    //                 { title: "รายระเอียด", defaultContent: "<a class='btndetail'>รายละเอียดเพิ่มเติม</a>" },
+    //             ]
+    //         })
+    //     }, error: (xhr) => {
+    //         alert(xhr.responseText);
+    //     }
+    // })
+
+    // var rowID;
+
+
+
+
 
     // Sidebar toggle behavior
     $(function () {
